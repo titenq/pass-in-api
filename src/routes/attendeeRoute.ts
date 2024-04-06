978566cb37113bff97e7becdff4993b2acac496c
titenq <titenq@gmail.com>
2024-04-06 11:58:15 -0300

    install biome js

--- src/routes/attendeeRoute.ts
+++ src/routes/attendeeRoute.ts
@@ -6,7 +6,7 @@ import AttendeeModel from '../models/attendeeModel';
 import {
   AttendeeBadgeRequestParams,
   AttendeeRequestBody,
-  AttendeeRequestParams
+  AttendeeRequestParams,
 } from '../interfaces/attendeeInterface';
 import prisma from '../lib/prisma';
 import generateCheckInId from '../helpers/generateCheckInId';
@@ -21,18 +21,23 @@ const attendeeRoute = async (fastify: FastifyInstance, options: any) => {
           summary: 'Buscar informações do participante pelo id',
           tags: ['attendee'],
           params: z.object({
-            attendeeId: z.coerce.number({
-              required_error: 'O parâmetro attendeeId é obrigatório',
-              invalid_type_error: 'O parâmetro attendeeId deve ser um número inteiro positivo'
-            }).int({
-              message: 'O parâmetro attendeeId deve ser um número inteiro'
-            }).positive({
-              message: 'O parâmetro attendeeId deve ser um número inteiro positivo'
-            }),
+            attendeeId: z.coerce
+              .number({
+                required_error: 'O parâmetro attendeeId é obrigatório',
+                invalid_type_error:
+                  'O parâmetro attendeeId deve ser um número inteiro positivo',
+              })
+              .int({
+                message: 'O parâmetro attendeeId deve ser um número inteiro',
+              })
+              .positive({
+                message:
+                  'O parâmetro attendeeId deve ser um número inteiro positivo',
+              }),
             checkInId: z.string({
               required_error: 'O parâmetro checkInId é obrigatório',
-              invalid_type_error: 'O parâmetro checkInId deve ser um texto'
-            })
+              invalid_type_error: 'O parâmetro checkInId deve ser um texto',
+            }),
           }),
           response: {
             200: z.object({
@@ -41,14 +46,14 @@ const attendeeRoute = async (fastify: FastifyInstance, options: any) => {
               email: z.string().email(),
               eventTitle: z.string().min(4),
               checkInURL: z.string().url(),
-              eventDate: z.date()
-            })
-          }
-        }
+              eventDate: z.date(),
+            }),
+          },
+        },
       },
       async (
         request: FastifyRequest<{ Params: AttendeeBadgeRequestParams }>,
-        reply: FastifyReply
+        reply: FastifyReply,
       ) => {
         try {
           const { attendeeId, checkInId } = request.params;
@@ -61,24 +66,29 @@ const attendeeRoute = async (fastify: FastifyInstance, options: any) => {
               event: {
                 select: {
                   title: true,
-                  eventDate: true
-                }
-              }
+                  eventDate: true,
+                },
+              },
             },
             where: {
               id: attendeeId,
-              checkInId
-            }
+              checkInId,
+            },
           });
 
           if (!attendee) {
-            return reply.status(404).send({ error: 'Participante não encontrado.' });
+            return reply
+              .status(404)
+              .send({ error: 'Participante não encontrado.' });
           }
 
           const protocol = request.protocol;
           const hostname = request.hostname;
           const baseURL = `${protocol}://${hostname}`;
-          const checkInURL = new URL(`attendees/${attendeeId}/check-in/${attendee.checkInId}`, baseURL).toString();
+          const checkInURL = new URL(
+            `attendees/${attendeeId}/check-in/${attendee.checkInId}`,
+            baseURL,
+          ).toString();
 
           const { event, ...rest } = attendee;
 
@@ -86,26 +96,28 @@ const attendeeRoute = async (fastify: FastifyInstance, options: any) => {
             ...rest,
             eventTitle: event.title,
             eventDate: event.eventDate,
-            checkInURL
+            checkInURL,
           };
 
           return reply.status(200).send(attendeeReply);
         } catch (error) {
           console.log('error: ', error);
         }
-      }).setErrorHandler((error, request, reply) => {
-        if (error instanceof ZodError) {
-          reply.status(400).send({
-            statusCode: 400,
-            error: 'Bad Request',
-            issues: error.issues,
-          });
+      },
+    )
+    .setErrorHandler((error, request, reply) => {
+      if (error instanceof ZodError) {
+        reply.status(400).send({
+          statusCode: 400,
+          error: 'Bad Request',
+          issues: error.issues,
+        });
 
-          return;
-        }
+        return;
+      }
 
-        reply.send(error);
-      });
+      reply.send(error);
+    });
 
   fastify
     .withTypeProvider<ZodTypeProvider>()
@@ -116,81 +128,92 @@ const attendeeRoute = async (fastify: FastifyInstance, options: any) => {
           summary: 'Fazer check-in do participante pelo id',
           tags: ['check-in'],
           params: z.object({
-            attendeeId: z.coerce.number({
-              required_error: 'O parâmetro attendeeId é obrigatório',
-              invalid_type_error: 'O parâmetro attendeeId deve ser um número inteiro positivo'
-            }).int({
-              message: 'O parâmetro attendeeId deve ser um número inteiro'
-            }).positive({
-              message: 'O parâmetro attendeeId deve ser um número inteiro positivo'
-            }),
+            attendeeId: z.coerce
+              .number({
+                required_error: 'O parâmetro attendeeId é obrigatório',
+                invalid_type_error:
+                  'O parâmetro attendeeId deve ser um número inteiro positivo',
+              })
+              .int({
+                message: 'O parâmetro attendeeId deve ser um número inteiro',
+              })
+              .positive({
+                message:
+                  'O parâmetro attendeeId deve ser um número inteiro positivo',
+              }),
             checkInId: z.string({
               required_error: 'O parâmetro checkInId é obrigatório',
-              invalid_type_error: 'O parâmetro checkInId deve ser um texto'
-            })
+              invalid_type_error: 'O parâmetro checkInId deve ser um texto',
+            }),
           }),
           response: {
             201: z.object({
               id: z.number().int().positive(),
               createdAt: z.date(),
               attendeeId: z.number().int().positive(),
-              checkInId: z.string()
-            })
-          }
-        }
+              checkInId: z.string(),
+            }),
+          },
+        },
       },
       async (
         request: FastifyRequest<{ Params: AttendeeBadgeRequestParams }>,
-        reply: FastifyReply
+        reply: FastifyReply,
       ) => {
         try {
           const { attendeeId, checkInId } = request.params;
 
           const attendee = await prisma.attendee.findUnique({
             where: {
-              id: attendeeId ,
-              checkInId
-            }
+              id: attendeeId,
+              checkInId,
+            },
           });
 
           if (!attendee) {
-            return reply.status(409).send({ error: 'Id e checkInId do participante não conferem.' });
+            return reply
+              .status(409)
+              .send({ error: 'Id e checkInId do participante não conferem.' });
           }
 
           const attendeeCheckIn = await prisma.checkIn.findUnique({
             where: {
-              attendeeId
-            }
+              attendeeId,
+            },
           });
 
           if (attendeeCheckIn) {
-            return reply.status(409).send({ error: 'Check In já realizado com o ID do participante.' });
+            return reply.status(409).send({
+              error: 'Check In já realizado com o ID do participante.',
+            });
           }
 
           const checkIn = await prisma.checkIn.create({
             data: {
               attendeeId,
-              checkInId
-            }
+              checkInId,
+            },
           });
 
           return reply.status(201).send(checkIn);
         } catch (error) {
           console.log('error: ', error);
         }
-      }).setErrorHandler((error, request, reply) => {
-        if (error instanceof ZodError) {
-          reply.status(400).send({
-            statusCode: 400,
-            error: 'Bad Request',
-            issues: error.issues,
-          });
-          
-          return;
-        }
+      },
+    )
+    .setErrorHandler((error, request, reply) => {
+      if (error instanceof ZodError) {
+        reply.status(400).send({
+          statusCode: 400,
+          error: 'Bad Request',
+          issues: error.issues,
+        });
 
-        reply.send(error);
-      });
+        return;
+      }
+
+      reply.send(error);
+    });
 
   fastify
     .withTypeProvider<ZodTypeProvider>()
@@ -200,32 +223,42 @@ const attendeeRoute = async (fastify: FastifyInstance, options: any) => {
         schema: {
           summary: 'Registrar participante pelo id do evento',
           tags: ['attendee'],
-          body: z.object({
-            name: z.string({
-              invalid_type_error: 'O campo nome deve ser um texto',
-              required_error: 'O campo nome é obrigatório'
-            }).min(4, {
-              message: 'O campo nome deve ter no mínimo 4 caracteres'
-            }).max(64, {
-              message: 'O campo nome dever ter no máximo 64 caracteres'
-            }),
-            email: z.string().email()
-          }).describe('name: string, mínimo 4 caracteres, máximo 64 caracteres\nemail: string'),
+          body: z
+            .object({
+              name: z
+                .string({
+                  invalid_type_error: 'O campo nome deve ser um texto',
+                  required_error: 'O campo nome é obrigatório',
+                })
+                .min(4, {
+                  message: 'O campo nome deve ter no mínimo 4 caracteres',
+                })
+                .max(64, {
+                  message: 'O campo nome dever ter no máximo 64 caracteres',
+                }),
+              email: z.string().email(),
+            })
+            .describe(
+              'name: string, mínimo 4 caracteres, máximo 64 caracteres\nemail: string',
+            ),
           params: z.object({
             eventId: z.string().uuid({
-              message: 'UUID inválida'
-            })
+              message: 'UUID inválida',
+            }),
           }),
           response: {
             201: z.object({
-              attendeeId: z.number().int().positive()
-            })
-          }
-        }
+              attendeeId: z.number().int().positive(),
+            }),
+          },
+        },
       },
       async (
-        request: FastifyRequest<{ Body: AttendeeRequestBody, Params: AttendeeRequestParams }>,
-        reply: FastifyReply
+        request: FastifyRequest<{
+          Body: AttendeeRequestBody;
+          Params: AttendeeRequestParams;
+        }>,
+        reply: FastifyReply,
       ) => {
         try {
           const { eventId } = request.params;
@@ -235,45 +268,48 @@ const attendeeRoute = async (fastify: FastifyInstance, options: any) => {
             where: {
               eventId_email: {
                 email,
-                eventId
-              }
-            }
+                eventId,
+              },
+            },
           });
 
           if (attendeeEmailEventId) {
             return reply.status(409).send({
-              error: 'Participante já resgistrado para esse evento.'
+              error: 'Participante já resgistrado para esse evento.',
             });
           }
 
           const [event, countAttendeesInEvent] = await Promise.all([
             prisma.event.findUnique({
               where: {
-                id: eventId
-              }
+                id: eventId,
+              },
             }),
             prisma.attendee.count({
               where: {
-                eventId
-              }
-            })
+                eventId,
+              },
+            }),
           ]);
 
           if (!event) {
             return reply.status(404).send({
-              error: 'Evento não encontrado.'
+              error: 'Evento não encontrado.',
             });
           }
 
           if (!event?.isActive) {
             return reply.status(409).send({
-              error: 'O evento está inativo.'
+              error: 'O evento está inativo.',
             });
           }
 
-          if (event?.maximumAttendees && countAttendeesInEvent >= event?.maximumAttendees) {
+          if (
+            event?.maximumAttendees &&
+            countAttendeesInEvent >= event?.maximumAttendees
+          ) {
             return reply.status(409).send({
-              error: 'O evento atingiu ao número máximo de participantes.'
+              error: 'O evento atingiu ao número máximo de participantes.',
             });
           }
 
@@ -282,8 +318,8 @@ const attendeeRoute = async (fastify: FastifyInstance, options: any) => {
               checkInId: generateCheckInId(),
               name,
               email,
-              eventId
-            }
+              eventId,
+            },
           });
 
           return reply.status(201).send({ attendeeId: attendee.id });
@@ -292,19 +328,21 @@ const attendeeRoute = async (fastify: FastifyInstance, options: any) => {
 
           return reply.status(501).send(error);
         }
-      }).setErrorHandler((error, request, reply) => {
-        if (error instanceof ZodError) {
-          reply.status(400).send({
-            statusCode: 400,
-            error: 'Bad Request',
-            issues: error.issues,
-          });
-          
-          return;
-        }
+      },
+    )
+    .setErrorHandler((error, request, reply) => {
+      if (error instanceof ZodError) {
+        reply.status(400).send({
+          statusCode: 400,
+          error: 'Bad Request',
+          issues: error.issues,
+        });
 
-        reply.send(error);
-      });
+        return;
+      }
+
+      reply.send(error);
+    });
 };
 
 export default attendeeRoute;
