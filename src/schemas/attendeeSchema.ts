236772f2a3900fd9602549ac7f9d9f16a66d49e5
titenq <titenq@gmail.com>
2024-04-12 23:21:19 -0300

    response message error

--- src/schemas/attendeeSchema.ts
+++ src/schemas/attendeeSchema.ts
@@ -7,29 +7,70 @@ const getAttendeeBadgeSchema = {
     attendeeId: z.coerce
       .number({
         required_error: 'O parâmetro attendeeId é obrigatório',
-        invalid_type_error:
-          'O parâmetro attendeeId deve ser um número inteiro positivo',
+        invalid_type_error: 'O parâmetro attendeeId deve ser um número inteiro positivo',
       })
       .int({
         message: 'O parâmetro attendeeId deve ser um número inteiro',
       })
       .positive({
-        message:
-          'O parâmetro attendeeId deve ser um número inteiro positivo',
+        message: 'O parâmetro attendeeId deve ser um número inteiro positivo',
       }),
-    checkInId: z.string({
-      required_error: 'O parâmetro checkInId é obrigatório',
-      invalid_type_error: 'O parâmetro checkInId deve ser um texto',
-    }),
+    checkInId: z
+      .string({
+        required_error: 'O parâmetro checkInId é obrigatório',
+        invalid_type_error: 'O parâmetro checkInId deve ser um texto',
+      }),
   }),
   response: {
     200: z.object({
-      checkInId: z.string(),
-      name: z.string().min(4),
-      email: z.string().email(),
-      eventTitle: z.string().min(4),
-      checkInURL: z.string().url(),
-      eventDate: z.date(),
+      checkInId: z
+        .string({
+          required_error: 'checkInId é obrigatório',
+          invalid_type_error: 'checkInId deve ser um texto',
+        }),
+      name: z
+        .string({
+          required_error: 'name é obrigatório',
+          invalid_type_error: 'name deve ser um texto',
+        })
+        .min(4, {
+          message: 'name deve ter no mínimo 4 caracteres'
+        })
+        .max(64, {
+          message: 'name deve ter no máximo 64 caracteres'
+        }),
+      email: z
+        .string({
+          required_error: 'email é obrigatório',
+          invalid_type_error: 'email deve ser um texto',
+        })
+        .email({
+          message: 'Formato de e-mail inválido'
+        }),
+      eventTitle: z
+        .string({
+          required_error: 'eventTitle é obrigatório',
+          invalid_type_error: 'eventTitle deve ser um texto',
+        })
+        .min(4, {
+          message: 'eventTitle deve ter no mínimo 4 caracteres'
+        })
+        .max(64, {
+          message: 'eventTitle deve ter no máximo 64 caracteres'
+        }),
+      checkInURL: z
+        .string({
+          required_error: 'checkInURL é obrigatório',
+          invalid_type_error: 'checkInURL deve ser um texto',
+        })
+        .url({
+          message: 'Formato de URL inválido'
+        }),
+      eventDate: z
+        .date({
+          required_error: 'eventDate é obrigatório',
+          invalid_type_error: 'eventDate com formato inválido'
+        }),
     }),
   }
 };
@@ -41,27 +82,54 @@ const getAttendeeCheckInSchema = {
     attendeeId: z.coerce
       .number({
         required_error: 'O parâmetro attendeeId é obrigatório',
-        invalid_type_error:
-          'O parâmetro attendeeId deve ser um número inteiro positivo',
+        invalid_type_error: 'O parâmetro attendeeId deve ser um número inteiro positivo',
       })
       .int({
         message: 'O parâmetro attendeeId deve ser um número inteiro',
       })
       .positive({
-        message:
-          'O parâmetro attendeeId deve ser um número inteiro positivo',
+        message: 'O parâmetro attendeeId deve ser um número inteiro positivo',
       }),
-    checkInId: z.string({
-      required_error: 'O parâmetro checkInId é obrigatório',
-      invalid_type_error: 'O parâmetro checkInId deve ser um texto',
-    }),
+    checkInId: z
+      .string({
+        required_error: 'O parâmetro checkInId é obrigatório',
+        invalid_type_error: 'O parâmetro checkInId deve ser um texto',
+      }),
   }),
   response: {
     201: z.object({
-      id: z.number().int().positive(),
-      createdAt: z.date(),
-      attendeeId: z.number().int().positive(),
-      checkInId: z.string(),
+      id: z
+        .number({
+          required_error: 'id é obrigatório',
+          invalid_type_error: 'id deve ser um número'
+        })
+        .int({
+          message: 'id deve ser um número inteiro'
+        })
+        .positive({
+          message: 'id deve ser um número positivo'
+        }),
+      createdAt: z
+        .date({
+          required_error: 'createdAt é obrigatório',
+          invalid_type_error: 'createdAt com formato inválido'
+        }),
+      attendeeId: z
+        .number({
+          required_error: 'attendeeId é obrigatório',
+          invalid_type_error: 'attendeeId deve ser um número'
+        })
+        .int({
+          message: 'attendeeId deve ser um número inteiro'
+        })
+        .positive({
+          message: 'attendeeId deve ser um número positivo'
+        }),
+      checkInId: z
+        .string({
+          required_error: 'checkInId é obrigatório',
+          invalid_type_error: 'checkInId deve ser um texto',
+        }),
     }),
   }
 };
@@ -82,21 +150,41 @@ const getAttendeeByEmailSchema = {
         .max(64, {
           message: 'O campo nome dever ter no máximo 64 caracteres',
         }),
-      email: z.string().email({
-        message: 'Formato de e-mail inválido',
-      }),
+      email: z
+        .string({
+          invalid_type_error: 'O campo email deve ser um texto',
+          required_error: 'O campo email é obrigatório',
+        })
+        .email({
+          message: 'Formato de e-mail inválido',
+        }),
     })
     .describe(
       'name: string, mínimo 4 caracteres, máximo 64 caracteres\nemail: string',
     ),
   params: z.object({
-    eventId: z.string().uuid({
-      message: 'UUID inválido',
-    }),
+    eventId: z
+      .string({
+        invalid_type_error: 'O campo eventId deve ser um texto',
+        required_error: 'O campo eventId é obrigatório',
+      })
+      .uuid({
+        message: 'UUID inválido',
+      }),
   }),
   response: {
     201: z.object({
-      attendeeId: z.number().int().positive(),
+      attendeeId: z
+        .number({
+          invalid_type_error: 'attendeeId deve ser um número',
+          required_error: 'attendeeId é obrigatório',
+        })
+        .int({
+          message: 'attendeeId deve ser um número inteiro'
+        })
+        .positive({
+          message: 'attendeeId deve ser um número positivo'
+        }),
     }),
   }
 };
--- src/schemas/eventSchema.ts
+++ src/schemas/eventSchema.ts
@@ -4,14 +4,19 @@ const getAttendeesByEventSchema = {
   summary: 'Buscar participantes pelo id do evento',
   tags: ['Eventos'],
   params: z.object({
-    eventId: z.string().uuid({
-      message: 'UUID inválido',
-    }),
+    eventId: z
+      .string({
+        required_error: 'eventId é obrigatório',
+        invalid_type_error: 'eventId deve ser um texto'
+      })
+      .uuid({
+        message: 'UUID inválido',
+      }),
   }),
   querystring: z.object({
     page: z.coerce
       .number({
-        invalid_type_error: 'O parâmetro page deve ser um número',
+        invalid_type_error: 'O parâmetro page deve ser um número inteiro positivo',
       })
       .int({
         message: 'O parâmetro page deve ser um número inteiro',
@@ -23,37 +28,106 @@ const getAttendeesByEventSchema = {
       .default(1),
     limit: z.coerce
       .number({
-        invalid_type_error:
-          'O parâmetro limit deve ser um número inteiro positivo',
+        invalid_type_error: 'O parâmetro limit deve ser um número inteiro positivo',
       })
       .int({
         message: 'O parâmetro limit deve ser um número inteiro',
       })
       .positive({
-        message:
-          'O parâmetro limit deve ser um número inteiro positivo',
+        message: 'O parâmetro limit deve ser um número inteiro positivo',
       })
       .nullish()
       .default(10),
-    query: z
-      .string({
-        invalid_type_error: 'O parâmetro query deve ser um texto',
-      })
+    query: z.string({
+      invalid_type_error: 'O parâmetro query deve ser um texto',
+    })
       .nullish(),
   }),
   response: {
     200: z.object({
-      currentPage: z.number().int().positive(),
-      totalPages: z.number().int().positive(),
-      totalAttendees: z.number().int().positive().nullish().default(0),
+      currentPage: z
+        .number({
+          required_error: 'currentPage é obrigatório',
+          invalid_type_error: 'currentPage deve ser um número inteiro positivo'
+        })
+        .int({
+          message: 'currentPage deve ser um número inteiro'
+        })
+        .positive({
+          message: 'currentPage deve ser um número inteiro positivo'
+        }),
+      totalPages: z
+        .number({
+          invalid_type_error: 'totalPages deve ser um número inteiro positivo'
+        })
+        .int({
+          message: 'totalPages deve ser um número inteiro'
+        })
+        .positive({
+          message: 'totalPages deve ser um número inteiro positivo'
+        }),
+      totalAttendees: z
+        .number({
+          invalid_type_error: 'totalAttendees deve ser um número inteiro não negativo'
+        })
+        .int({
+          message: 'totalAttendees deve ser um número inteiro'
+        })
+        .nonnegative({
+          message: 'totalAttendees não pode ser um número negativo'
+        })
+        .nullish()
+        .default(0),
       attendees: z.array(
         z.object({
-          id: z.number().int().positive(),
-          name: z.string().min(4),
-          email: z.string().email(),
-          createdAt: z.date(),
-          eventId: z.string().uuid(),
-          checkInAt: z.date().nullish(),
+          id: z
+            .number({
+              required_error: 'id é obrigatório',
+              invalid_type_error: 'id deve ser um número inteiro positivo'
+            })
+            .int({
+              message: 'id deve ser um número inteiro'
+            })
+            .positive({
+              message: 'id deve ser um número positivo'
+            }),
+          name: z
+            .string({
+              required_error: 'name é obrigatório',
+              invalid_type_error: 'name deve ser um texto'
+            })
+            .min(4, {
+              message: 'name deve ter no mínimo 4 caracteres'
+            })
+            .max(64, {
+              message: 'name deve ter no máximo 64 caracteres'
+            }),
+          email: z
+            .string({
+              required_error: 'email é obrigatório',
+              invalid_type_error: 'email deve ser uma string'
+            })
+            .email({
+              message: 'Formato de e-mail inválido'
+            }),
+          createdAt: z
+            .date({
+              required_error: 'createdAt é obrigatório',
+              invalid_type_error: 'createdAt com formato inválido'
+            }),
+          eventId: z
+            .string({
+              required_error: 'eventId é obrigatório',
+              invalid_type_error: 'eventId deve ser um texto'
+            })
+            .uuid({
+              message: 'UUID inválido'
+            }),
+          checkInAt: z
+            .date({
+              invalid_type_error: 'checkInAt com formato inválido'
+            })
+            .nullish(),
         }),
       ),
     }),
@@ -64,27 +138,77 @@ const getEventByIdSchema = {
   summary: 'Buscar evento por id',
   tags: ['Eventos'],
   params: z.object({
-    eventId: z.string().uuid({
-      message: 'UUID inválido',
-    }),
+    eventId: z
+      .string({
+        required_error: 'eventId é obrigatório',
+        invalid_type_error: 'eventId deve ser um texto'
+      })
+      .uuid({
+        message: 'UUID inválido',
+      }),
   }),
   response: {
     200: z.object({
-      id: z.string().uuid(),
-      title: z.string(),
-      details: z.string().nullish(),
-      maximumAttendees: z.number().int().positive().nullish(),
-      isActive: z.boolean(),
-      slug: z.string(),
-      createdAt: z.date(),
-      attendeesAmount: z.number({
-        invalid_type_error: 'attendeesAmount deve ser um número',
-      }).int({
-        message: 'attendeesAmount deve ser um número inteiro'
-      }).positive({
-        message: 'attendeesAmount deve ser um número positivo'
-      }).default(0),
-      eventDate: z.date()
+      id: z
+        .string({
+          required_error: 'id é obrigatório',
+          invalid_type_error: 'id deve ser um texto'
+        })
+        .uuid({
+          message: 'UUID inválido',
+        }),
+      title: z
+        .string({
+          required_error: 'title é obrigatório',
+          invalid_type_error: 'title deve ser um texto'
+        }),
+      details: z
+        .string({
+          invalid_type_error: 'details deve ser um texto'
+        })
+        .nullish(),
+      maximumAttendees: z
+        .number({
+          invalid_type_error: 'maximumAttendees deve ser um número'
+        })
+        .int({
+          message: 'maximumAttendees deve ser um número inteiro'
+        })
+        .positive({
+          message: 'maximumAttendees deve ser um número inteiro positivo'
+        })
+        .nullish(),
+      isActive: z
+        .boolean({
+          required_error: 'isActive é obrigatório',
+          invalid_type_error: 'isActive deve ser um booleano'
+        }),
+      slug: z
+        .string({
+          required_error: 'slug é obrigatório',
+          invalid_type_error: 'slug deve ser um texto'
+        }),
+      createdAt: z
+        .date({
+          required_error: 'createdAt é obrigatório',
+          invalid_type_error: 'createdAt com formato inválido'
+        }),
+      attendeesAmount: z
+        .number({
+          invalid_type_error: 'attendeesAmount deve ser um número',
+        })
+        .int({
+          message: 'attendeesAmount deve ser um número inteiro'
+        })
+        .nonnegative({
+          message: 'attendeesAmount não pode ser um número negativo'
+        })
+        .default(0),
+      eventDate: z
+        .date({
+          required_error: 'eventDate é obrigatório',
+          invalid_type_error: 'eventDate com formato inválido'
+        })
     }),
   }
 };
@@ -92,54 +216,58 @@ const getEventByIdSchema = {
 const createEventSchema = {
   summary: 'Criar evento',
   tags: ['Eventos'],
-  body: z
-    .object({
-      title: z
-        .string({
-          required_error: 'O campo title é obrigatório',
-          invalid_type_error: 'O campo title deve ser um texto',
-        })
-        .min(4, {
-          message: 'O campo title deve ter no mínimo 4 caracteres',
-        })
-        .max(64, {
-          message: 'O campo title deve ter no máximo 64 caracteres',
-        }),
-      details: z
-        .string({
-          invalid_type_error: 'O campo details deve ser um texto',
-        })
-        .nullable(),
-      maximumAttendees: z
-        .number({
-          required_error: 'O campo maximumAttendees é obrigatório',
-          invalid_type_error:
-            'O campo maximumAttendees deve ser um número inteiro positivo',
-        })
-        .int({
-          message:
-            'O campo maximumAttendees deve ser um número inteiro',
-        })
-        .positive({
-          message:
-            'O campo maximumAttendees deve ser um número inteiro positivo',
-        })
-        .nullable(),
-      isActive: z.boolean({
+  body: z.object({
+    title: z
+      .string({
+        required_error: 'O campo title é obrigatório',
+        invalid_type_error: 'O campo title deve ser um texto',
+      })
+      .min(4, {
+        message: 'O campo title deve ter no mínimo 4 caracteres',
+      })
+      .max(64, {
+        message: 'O campo title deve ter no máximo 64 caracteres',
+      }),
+    details: z
+      .string({
+        invalid_type_error: 'O campo details deve ser um texto',
+      })
+      .nullish(),
+    maximumAttendees: z
+      .number({
+        invalid_type_error: 'O campo maximumAttendees deve ser um número',
+      })
+      .int({
+        message: 'O campo maximumAttendees deve ser um número inteiro',
+      })
+      .positive({
+        message: 'O campo maximumAttendees deve ser um número inteiro positivo',
+      })
+      .nullish(),
+    isActive: z
+      .boolean({
         required_error: 'O campo isActive é obrigatório',
         invalid_type_error: 'O campo isActive deve ser um boolean',
       }),
-      eventDate: z.string().datetime({
-        message:
-          'O campo eventDate está com o formato de data inválido',
+    eventDate: z
+      .date({
+        required_error: 'eventDate é obrigatório',
+        invalid_type_error: 'eventDate com formato inválido'
       }),
-    })
+  })
     .describe(
       'title: string, mínimo 4 caracteres, máximo 64 caracteres\ndetails: string, não obrigatório\nmaximumAttendees: número inteiro positivo, não obrigatório\nisActive: boolean\neventDate: string no formato 2024-04-18T22:30:00Z',
     ),
   response: {
     201: z.object({
-      eventId: z.string().uuid(),
+      eventId: z
+        .string({
+          required_error: 'eventId é obrigatório',
+          invalid_type_error: 'eventId deve ser um texto',
+        })
+        .uuid({
+          message: 'UUID inválido'
+        }),
     }),
   }
 };
